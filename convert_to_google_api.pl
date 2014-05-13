7e107f8bc5c294a0e54cfb3b1eec348c57fb6f33
Célestin Matte <celestin.matte@gmail.com>
2014-05-13 12:07:31 +0200

    New available format for convert_to_google_api

--- convert_to_google_api.pl
+++ convert_to_google_api.pl
@@ -11,21 +11,25 @@ use Getopt::Std;
 
 our $opt_a = 0;
 our $opt_m = 0;
+our $opt_u = 0;
 
-getopts('am');
+getopts('amu');
 my $filename = shift;
 
 if (!defined $filename) {
-        print "Usage: convert_to_google_api.pl [-a|-m] <filename>\n";
+        print "Usage: convert_to_google_api.pl [-a|-m|-u] <filename>\n";
 	print "-a: format for aircrack-ng (default)\n";
 	print "-m: format for mdk3\n";
+	print "-u: format with mac addresses only\n";
         exit 1;
 }
 
-if ($opt_a == 0 && $opt_m == 0) {
+if ($opt_a == 0 && $opt_m == 0 && $opt_u == 0) {
 	$opt_a = 1;
 }
-if ($opt_a == $opt_m) {
+if (($opt_a == 1 && $opt_m == 1) ||
+	($opt_a == 1 && $opt_u == 1) ||
+	    ($opt_u == 1 && $opt_m == 1)) {
 	print STDERR "Error: you have to select only one format\n";
 	exit 1;
 }
@@ -45,8 +49,12 @@ while (<$file>) {
 		if (/^([A-F0-9:]+) (\d+) /) {
 			print "  {\n    'macAddress': '$1',\n    'channel': $2\n  }";
 		}
-	} else {
+	} elsif ($opt_a) {
 		if (/^([A-F0-9:]{17}):/) {
+			print "  {\n    'macAddress': '$1'\n  }";
+		}
+	} else {
+		if (/^([a-fA-F0-9:]+)/) {
 			print "  {\n    'macAddress': '$1'\n  }";
 		}
 	}
